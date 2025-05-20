e754ef96ba376f7f7d1b1ac87582af52e24c894d
Hammad Mehmood <hammad.mehmood898@gmail.com>
2025-05-20 21:16:30 +0500

    Phase 4 Complete: - Tailwind CSS used( no need for scoped styles as of yet) - Todo Type was already defined in \features\todos\TodoTaskTypes.ts - Also added reuseability by seperately creating in Ui folder

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,5 +1,6 @@
 import { type PropsWithChildren } from 'react';
 import { type Task } from '../../features/todos/TodoTaskTypes';
+import Button from '../ui/Button';
 
 type TodoItemProps = PropsWithChildren<{
   task: Task;
@@ -10,32 +11,19 @@ type TodoItemProps = PropsWithChildren<{
 export default function TodoItem({ task, onDelete, onToggle, children }: TodoItemProps) {
   return (
     <li className="flex items-center gap-2 py-2 border-b">
-      <input 
-        type="checkbox" 
+      <input
+        type="checkbox"
         checked={task.isCompleted}
         onChange={() => onToggle(task.id)}
         className="h-5 w-5"
       />
-      <span className={task.isCompleted ? "line-through text-gray-500" : ""}>
-        {task.title}
-      </span>
-      <button 
+      <span className={task.isCompleted ? 'line-through text-gray-500' : ''}>{task.title}</span>
+      <Button
         onClick={() => onDelete(task.id)}
         className="ml-auto bg-red-500 hover:bg-red-700 text-white text-sm py-1 px-2 rounded"
       >
         Delete
-      </button>
-      <span className="text-sm text-gray-500  border-l pl-2">
-        {task.priority === 'p1' ? (
-          <span className="text-green-500">Low</span>
-        ) : task.priority === 'p2' ? (
-          <span className="text-yellow-500">Medium</span>
-        ) : task.priority === 'p3' ? (
-          <span className="text-red-500">High</span>
-        ) : (
-          <span className="text-gray-500">Unknown</span>
-        )}
-      </span>
+      </Button>
       {children}
     </li>
   );
